bc2334afc338926b7e3aa87ea8065904798cf72c
Ethan Olsen <ethan@crowdhubapps.com>
2023-01-03 04:19:48 -0800

    feat(friends): add base logic for friends status and improve add friend organization

--- src/routes/friend/add/+page.svelte
+++ src/routes/friend/add/+page.svelte
@@ -2,48 +2,84 @@
 	import LabeledTextInput from '$lib/inputs/LabeledTextInput.svelte';
 	import Avatar from '$lib/profile/Avatar.svelte';
 	import { supabase } from '$lib/supabaseClient';
+	import { FriendsStatus } from '$types/friends-status.d';
 	import type { Database } from '$types/supabase';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 
-	type Profile = Database['public']['Tables']['profiles']['Row'];
+	type Profile = Database['public']['Tables']['profiles']['Row'] & {
+		incoming_requests:
+			| {
+					id: number;
+					status: string | null;
+			  }[]
+			| null;
+	} & {
+		outgoing_requests:
+			| {
+					id: number;
+					status: string | null;
+			  }[]
+			| null;
+	};
 
 	$: ({ profile } = data);
 
 	let searchText = '';
-	let searchedFriends: Profile[] = [];
+	let searchedFriends: Awaited<ReturnType<typeof searchFriends>> = [];
 	let friendsLoading = false;
 
+	const searchFriends = async (search_text: string) => {
+		const { data: newSearchedFriends } = await supabase
+			.from('profiles')
+			.select(
+				`*, outgoing_requests:friends!friends_user_id_fkey(id, status), incoming_requests:friends!friends_requester_id_fkey(id, status)`
+			)
+			.ilike('username', `%${search_text}%`)
+			.neq('id', profile?.id);
+		return newSearchedFriends;
+	};
+
+	const handleSearch = async (value: string) => {
+		if (!value) {
+			searchedFriends = [];
+			return;
+		}
+		friendsLoading = true;
+		const newSearchedFriends = await searchFriends(value);
+		searchedFriends = newSearchedFriends ?? [];
+		friendsLoading = false;
+		console.log({ searchedFriends });
+	};
+
 	let timer: NodeJS.Timeout;
+
 	const debounce = (value: string) => {
 		clearTimeout(timer);
-		timer = setTimeout(async () => {
-			if (!value) {
-				searchedFriends = [];
-				return;
-			}
-			friendsLoading = true;
-			const { data: newSearchedFriends } = await supabase
-				.from('profiles')
-				.select(`*, friends!friends_requester_id_fkey(id)`)
-				.ilike('username', `%${value}%`);
-			searchedFriends = newSearchedFriends ?? [];
-			friendsLoading = false;
-			console.log({ searchedFriends });
-		}, 750);
+		timer = setTimeout(() => handleSearch(value), 750);
 	};
 
 	$: debounce(searchText);
 
-	const addFriend = async () => {
+	const sendRequest = async (user_id: string) => {
 		try {
-			const { data: userProfile, error: userProfileError } = await supabase
-				.from('profiles')
-				.select(`id`)
-				.eq('username', searchText)
-				.single();
+			const { data: userProfile, error: userProfileError } = await supabase.from('friends').upsert({
+				requester_id: profile?.id,
+				user_id,
+				status: FriendsStatus.PENDING
+			});
+			console.log({ userProfile, userProfileError });
 		} catch (error) {}
+	};
+
+	const isRequestStatus = (
+		requests: NonNullable<typeof searchedFriends>[0]['outgoing_requests'],
+		status: FriendsStatus
+	) => {
+		return requests && Array.isArray(requests)
+			? requests?.some((request) => request.status === status)
+			: typeof requests === 'object' && requests?.status === status;
 	};
 </script>
 
@@ -57,16 +93,54 @@
 	/>
 </label>
 
-{#each searchedFriends as { full_name, username, avatar_url }}
-	<div class="flex gap-2">
-		<div class="w-fit">
-			<Avatar {full_name} {avatar_url} />
+{#each searchedFriends ?? [] as { id, full_name, username, avatar_url, incoming_requests, outgoing_requests }}
+	<!-- is already friend -->
+	{#if isRequestStatus(outgoing_requests, FriendsStatus.ACCEPTED)}
+		<a href="/chat/{id}" class="flex gap-2">
+			<div class="w-fit">
+				<Avatar {full_name} {avatar_url} />
+			</div>
+			<span class="flex-grow">@{username}</span>
+			<div>
+				<button on:click={() => sendRequest(id)} class="btn btn-sm btn-primary">Add</button>
+			</div>
+		</a>
+		<!-- has outgoing request from user -->
+	{:else if isRequestStatus(outgoing_requests, FriendsStatus.PENDING)}
+		<div class="flex gap-2">
+			<div class="w-fit">
+				<Avatar {full_name} {avatar_url} />
+			</div>
+			<span class="flex-grow">@{username}</span>
+			<div>
+				<span class="opacity-50">Requested</span>
+			</div>
 		</div>
-		<span class="flex-grow">@{username}</span>
-		<div>
-			<button on:click={addFriend} class="btn btn-sm btn-primary">Add</button>
+		<!-- has incoming request from user -->
+	{:else if isRequestStatus(incoming_requests, FriendsStatus.PENDING)}
+		<div class="flex gap-2">
+			<div class="w-fit">
+				<Avatar {full_name} {avatar_url} />
+			</div>
+			<span class="flex-grow">@{username}</span>
+			<div>
+				<button class="btn btn-sm btn-primary">Add</button>
+				<button class="btn btn-sm btn-error">Deny</button>
+			</div>
 		</div>
-	</div>
+		<!-- unconnected friend -->
+	{:else}
+		<div class="flex gap-2">
+			<div class="w-fit">
+				<Avatar {full_name} {avatar_url} />
+			</div>
+			<span class="flex-grow">@{username}</span>
+			<div>
+				<button on:click={() => sendRequest(id)} class="btn btn-sm btn-primary">Send Request</button
+				>
+			</div>
+		</div>
+	{/if}
 {:else}
 	<p>No friends found</p>
 {/each}
